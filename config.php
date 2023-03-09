e1b3c2783d8e481a1cab468f27c2affc55b49b08
Taheraldev <101641519+Taheraldev@users.noreply.github.com>
2023-03-09 16:27:15 +0300

    Update and rename config.phpp to config.php

--- config.php
+++ config.php
@@ -3,10 +3,10 @@ require_once 'system/Telegram.php';
 require_once 'system/Database.php';
 require_once 'system/Core.php';
 
-const _TOKEN = "bot-token";
+const _TOKEN = "5053664176:AAG_BKV_zGwIVACVBXGz5m3sxHEOsQ_B39Q";
 const _BotName = "AgeBirthBot";
 
-const _TimeStamp = "1605643011";
+const _TimeStamp = "5198110160";
 const _ZARCHI_CHANNEL = "chat-id";
 const _REPORT_CHANNEL = "chat-id";
 
